a244bf94fabde43e162a3a58942ecdaa145ff622
Thiago Dantas <thiagodantas.dev@gmail.com>
2026-04-05 00:30:08 -0300

    feat: standardize JSON responses for auth and authorization exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,5 +17,15 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 401);
+        });
+
+        $exceptions->render(function (AuthorizationException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 403);
+        });
     })->create();
